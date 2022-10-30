082cf329a985af6094b29a167aacacc4c8ea6be0
ᴍʏʟᴏᴠᴇᴍᴏᴏɴ <90930470+MyLoveMoon@users.noreply.github.com>
2022-10-30 11:38:52 +0530

    Add files via upload

---
+++ GrootMusic/plugins/modules/id.py
@@ -0,0 +1,21 @@
+# GROOT MUSIC NETWORK
+# G-Network Music Projects
+# Copyright (C) 2022 By @Groot_Network
+# Copy Rights @RJbr0 , @MyNameIsGROOT
+# Don't Any Value In This Repo If You Edit Your Github Will Get Banned 😌
+
+from GrootMusic import app
+from pyrogram import filters
+
+
+@app.on_message(filters.command("id"))
+def ids(_, message):
+    reply = message.reply_to_message
+    if reply:
+        message.reply_text(
+            f"**ʏᴏᴜʀ ɪᴅ**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ɪᴅ**: `{reply.from_user.id}`\n**ᴄʜᴀᴛ ɪᴅ**: `{message.chat.id}`"
+        )
+    else:
+        message.reply(
+            f"**ʏᴏᴜʀ ɪᴅ**: `{message.from_user.id}`\n**ᴄʜᴀᴛ ɪᴅ**: `{message.chat.id}`"
+        )
