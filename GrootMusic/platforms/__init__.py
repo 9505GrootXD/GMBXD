3e5ea60aad5430765f9bc64f1f6f0aee44939b8d
ᴍʏʟᴏᴠᴇᴍᴏᴏɴ <90930470+MyLoveMoon@users.noreply.github.com>
2022-10-30 11:31:22 +0530

    Add files via upload

---
+++ GrootMusic/platforms/__init__.py
@@ -0,0 +1,13 @@
+# GROOT MUSIC NETWORK
+# G-Network Music Projects
+# Copyright (C) 2022 By @Groot_Network
+# Copy Rights @RJbr0 , @MyNameIsGROOT
+# Don't Any Value In This Repo If You Edit Your Github Will Get Banned 😌
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
