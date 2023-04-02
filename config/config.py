0e8e3bcb226170c55da934345aa74a8e4c180f67
ɢʀᴏᴏᴛ xᴅ <128709493+9505GrootXD@users.noreply.github.com>
2023-04-02 22:17:23 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -38,7 +38,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MyLoveMoon/skyliteweech",
+    "https://github.com/9505GrootXD/GMBXD",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
